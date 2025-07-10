8f6d7c13a6d51fde279809f73e19bc87e1c039c6
XiaJayden <xia.jayden@gmail.com>
2025-07-09 18:59:10 -0700

    torch testing

---
+++ transformerstest.py
@@ -0,0 +1,8 @@
+# # Use a pipeline as a high-level helper
+# from transformers import pipeline
+
+# pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B")
+# pipe("Question: What color is the sky? Answer: ")
+
+import numpy
+print(numpy.version.version)
