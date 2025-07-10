7a07d6da0592d6cae56464fcfbd99ab3f1a9d2bf
XiaJayden <xia.jayden@gmail.com>
2025-07-09 19:58:50 -0700

    Update transformerstest.py

--- transformerstest.py
+++ transformerstest.py
@@ -1,8 +1,5 @@
-# # Use a pipeline as a high-level helper
-# from transformers import pipeline
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
-# pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B")
-# pipe("Question: What color is the sky? Answer: ")
-
-import numpy
-print(numpy.version.version)
+pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B")
+pipe("Question: What color is the sky? Answer: ")
